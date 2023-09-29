140cb59572f5ffc6dc99536a05759c412f0d9100
Tiago de Oliveira <deoliv.tiago@gmail.com>
2023-09-29 14:08:42 -0300

    chore: init spring boot app

---
+++ src/main/java/io/bourdox/clarxapi/ClarxAPIApplication.java
@@ -0,0 +1,13 @@
+package io.bourdox.clarxapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClarxAPIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClarxAPIApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/bourdox/clarxapi/ClarxAPIApplicationTests.java
@@ -0,0 +1,13 @@
+package io.bourdox.clarxapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClarxAPIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
